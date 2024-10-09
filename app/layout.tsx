c77ad7b34755f86ee74f7a3214938c1722de1ddc
MD. ABU SAYED <hello@sayed.page>
2024-10-09 13:55:25 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,18 +48,16 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} flex flex-col min-h-screen`}>
         <Navbar />
-        <main>{children}</main>
+        <main className="flex-grow">{children}</main>
         <Footer />
       </body>
     </html>
   );
+}
+
 }
